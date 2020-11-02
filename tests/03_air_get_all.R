513d54378767794b11abdda135c1bd95f84a127b
Viktor Horvath <viktor.horvath@wyss.harvard.edu>
2020-11-02 15:23:43 -0500

    Roxygen2 standard compliant package loading

--- tests/03_air_get_all.R
+++ tests/03_air_get_all.R
@@ -3,6 +3,6 @@
 # Check result variable for correct content
 
 source('tests/00_setup.R')
-source('R/airtabler.R')
+devtools::load_all()
 
 result = air_get_all(base = TEST_BASE, table_name = TEST_TABLE)
